30b000bc532f0e982ce121bfcad2951e372f9df9
Neelabh Singh <neelabh041997@gmail.com>
2024-05-07 12:53:40 +0530

    Changes in the github auth and home page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -8,13 +8,13 @@ import SortRepos from "../components/SortRepos";
 import Spinner from "../components/Spinner";
 
 const HomePage = () => {
-	const [userProfile, setUserProfile] = useState(null);
+	const [userProfile, setUserProfile] = useState({});
 	const [repos, setRepos] = useState([]);
 	const [loading, setLoading] = useState(false);
 
 	const [sortType, setSortType] = useState("recent");
 
-	const getUserProfileAndRepos = useCallback(async (username = "burakorkmez") => {
+	const getUserProfileAndRepos = useCallback(async (username = "neelabh04") => {
 		setLoading(true);
 		try {
 			const res = await fetch(`/api/users/profile/${username}`);
